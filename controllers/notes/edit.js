7dbed9d33c3ddb333de69c9e4970c96d5d7acb05
Andrew Danger Lyon <orthecreedence@gmail.com>
2013-04-24 11:01:35 -0700

    all changes to the note in the note edit screen now happen to a COPY of the note model (copied at edit init). only when a success save happens (ie success from the API) does the data from the edit get set back into the original note.

--- controllers/notes/edit.js
+++ controllers/notes/edit.js
@@ -21,11 +21,14 @@ var NoteEditController = Composer.Controller.extend({
 
 	project: null,
 	note: null,
+	note_copy: null,
 	tag_controller: null,
 
 	init: function()
 	{
 		if(!this.note) this.note = new Note({type: 'quick'});
+		// clone the note so any changes to it pre-save don't show up in the listings.
+		this.note_copy = this.note.clone();
 		this.render();
 		modal.open(this.el);
 		var close_fn = function() {
@@ -35,7 +38,7 @@ var NoteEditController = Composer.Controller.extend({
 		modal.addEvent('close', close_fn);
 		this.tag_controller = new NoteEditTagController({
 			inject: this.tags,
-			note: this.note,
+			note: this.note_copy,
 			project: this.project
 		});
 		this.select_tab(this.note.get('type'));
@@ -52,7 +55,7 @@ var NoteEditController = Composer.Controller.extend({
 	render: function()
 	{
 		var content = Template.render('notes/edit', {
-			note: toJSON(this.note),
+			note: toJSON(this.note_copy),
 			project: toJSON(this.project)
 		});
 		this.html(content);
@@ -62,7 +65,7 @@ var NoteEditController = Composer.Controller.extend({
 	{
 		if(e) e.stop();
 
-		if(this.note.get('type') == 'quick')
+		if(this.note_copy.get('type') == 'quick')
 		{
 			// do some basic guessing/intelligence stuff
 			var val = this.inp_quick.get('value');
@@ -72,7 +75,7 @@ var NoteEditController = Composer.Controller.extend({
 				if(val.match(/\.(jpg|jpeg|gif|png|tiff|bmp)([\w?&=#]+)?$/i))
 				{
 					// it's an image
-					this.note.set({
+					this.note_copy.set({
 						type: 'image',
 						url: val
 					});
@@ -80,7 +83,7 @@ var NoteEditController = Composer.Controller.extend({
 				else
 				{
 					// just a stupid link
-					this.note.set({
+					this.note_copy.set({
 						type: 'link',
 						url: val
 					});
@@ -89,7 +92,7 @@ var NoteEditController = Composer.Controller.extend({
 			else
 			{
 				// only other option is text for now
-				this.note.set({
+				this.note_copy.set({
 					type: 'text',
 					text: val,
 				});
@@ -97,17 +100,17 @@ var NoteEditController = Composer.Controller.extend({
 		}
 		else
 		{
-			switch(this.note.get('type'))
+			switch(this.note_copy.get('type'))
 			{
 			case 'link':
-				this.note.set({
+				this.note_copy.set({
 					url: this.inp_url.get('value'),
 					title: this.inp_title.get('value'),
 					text: this.inp_text.get('value')
 				});
 				break;
 			case 'image':
-				this.note.set({
+				this.note_copy.set({
 					url: this.inp_url.get('value'),
 					title: this.inp_title.get('value'),
 					text: this.inp_text.get('value')
@@ -115,23 +118,25 @@ var NoteEditController = Composer.Controller.extend({
 				break;
 			case 'text':
 			default:
-				this.note.set({
+				this.note_copy.set({
 					text: this.inp_text.get('value')
 				});
 				break;
 			}
 		}
 
+		var isnew	=	this.note_copy.is_new();
+		if(!this.note_copy.get('project_id'))
+			this.note_copy.set({project_id: this.project.id()});
+
+		// save the note copy, and on success, set the resulting data back into
+		// the original note (not the copy)
 		tagit.loading(true);
-
-		var isnew	=	this.note.is_new();
-		if(!this.note.get('project_id'))
-			this.note.set({project_id: this.project.id()});
-
-		this.note.save({
-			success: function() {
+		this.note_copy.save({
+			success: function(note_data) {
 				modal.close();
 				tagit.loading(false);
+				this.note.set(note_data);
 				if(isnew) this.project.get('notes').add(this.note);
 			}.bind(this),
 			error: function() {
