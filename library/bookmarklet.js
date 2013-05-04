9146fa06fb7a1d8feea7138f876efa1dfe519985
Andrew Danger Lyon <orthecreedence@gmail.com>
2013-05-03 23:39:24 -0700

    bookmarklet looks for og:image tags, and adds a markdown image formatter if found. works great.

--- library/bookmarklet.js
+++ library/bookmarklet.js
@@ -2,12 +2,22 @@ function _t_bookmarklet()
 {
 	var u = encodeURIComponent(window.location.href);
 	var t = encodeURIComponent(document.title);
-	var m = encodeURIComponent(document.getElementsByTagName('meta'));
+	var m = document.getElementsByTagName('meta');
 	var d = '';
-	for(x in m) { if(m[x].name == 'description') { d = m[x].content; break; } };
+	var i = false;
+	for(var x in m) {if(m[x].name == 'description') {d = m[x].content; break;}}
+	for(var x in m)
+	{
+		if(m[x].getAttribute && m[x].getAttribute('property') == 'og:image')
+		{
+			i = m[x].content; break;
+		}
+	}
 
-	/* TODO: remember to change "http://127..." to <?=__site_url?> in header bar */
-    f = 'http://127.0.0.1:81/bookmark?url='+ u +'&title='+ t +'&text='+ d;
+	if(i) d += '  \\n![image]('+i+')';
+	d = encodeURIComponent(d);
+
+    f = '<?=__site_url?>/bookmark?url='+ u +'&title='+ t +'&text='+ d;
 	t = function()
 	{
 		if(!window.open(f, 'tagit', 'location=yes,links=no,scrollbars=no,toolbar=no,width=740,height=525'))
