c0dd31c025fe446f851f22acdb234acfcd3492e5
Andrew Danger Lyon <orthecreedence@gmail.com>
2013-06-30 15:29:13 -0700

    adding menu

--- controllers/_modules/header_bar.js
+++ controllers/_modules/header_bar.js
@@ -1,15 +1,33 @@
 var HeaderBarController = Composer.Controller.extend({
 	inject: 'header',
 
+	elements: {
+		'ul.menu': 'menu'
+	},
+
+	events: {
+		'click a.menu': 'toggle_menu',
+		'click li.bookmarklet a': 'bookmarklet',
+		'mouseenter ul.menu': 'cancel_close_menu',
+		'mouseleave ul.menu': 'close_menu'
+	},
+
+	close_timer: null,
+
 	init: function()
 	{
 		tagit.user.bind(['login', 'logout'], this.render.bind(this), 'header_bar:user:render');
 		this.render();
+		this.close_timer = new Timer(250);
+		this.close_timer.end = function() {
+			this.menu.removeClass('open');
+		}.bind(this);
 	},
 
 	release: function()
 	{
 		tagit.user.unbind(['login', 'logout'], 'header_bar:user:render');
+		this.close_timer.end = null;
 		this.parent.apply(this, arguments);
 	},
 
@@ -19,5 +37,34 @@ var HeaderBarController = Composer.Controller.extend({
 			user: toJSON(tagit.user)
 		});
 		this.html(content);
+	},
+
+	toggle_menu: function(e)
+	{
+		if(e) e.stop();
+		if(this.menu.hasClass('open'))
+		{
+			this.menu.removeClass('open');
+		}
+		else
+		{
+			this.menu.addClass('open');
+		}
+	},
+
+	close_menu: function(e)
+	{
+		this.close_timer.start();
+	},
+
+	cancel_close_menu: function(e)
+	{
+		this.close_timer.stop();
+	},
+
+	bookmarklet: function(e)
+	{
+		if(e) e.stop();
+		alert('Drag me to your bookmarks!');
 	}
 });
