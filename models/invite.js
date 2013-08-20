04291507f07138ce6539150d1ec0640ca2d6ebbe
Andrew Danger Lyon <orthecreedence@gmail.com>
2013-08-19 20:24:41 -0700

    splitting out key encrypt/decrypt in invite model. also, adding lock icon (mainly for addon, but w/e)

--- models/invite.js
+++ models/invite.js
@@ -1,4 +1,57 @@
 var Invite = Composer.Model.extend({
+	encrypt_key: function(key, secret, options)
+	{
+		options || (options = {});
+		secret || (secret = false);
+
+		if(typeOf(secret) == 'string')
+		{
+			secret	=	secret.clean();
+			// remember, this is insecure already, so making the secret actually
+			// easy to enter is more of a priority
+			if(options.normalize) secret = secret.toLowerCase();
+			if(secret == '') secret = false;
+		}
+
+		// create a key used to encrypt the board's key before emailing it
+		var salt			=	(secret || '') + ':throw the NSA down the well';
+		var encrypting_pass	=	tcrypt.uuid();
+		var encrypting_key	=	tcrypt.key(encrypting_pass, salt, {keySize: 256/32, iterations: 400});
+
+		// don't do encryption directly, use the Protected model.
+		var keymodel		=	new InviteKey({key: tcrypt.key_to_string(key)});
+		keymodel.key		=	encrypting_key;
+		return {
+			encrypted_key: keymodel.toJSON().body,
+			encrypting_pass: encrypting_pass,
+			used_secret: !!secret
+		};
+	},
+
+	decrypt_key: function(encrypted_key, encrypting_pass, secret, options)
+	{
+		options || (options = {});
+		secret || (secret = false);
+
+		if(typeOf(secret) == 'string')
+		{
+			secret	=	secret.clean();
+			// remember, this is insecure already, so making the secret actually
+			// easy to enter is more of a priority
+			if(options.normalize) secret = secret.toLowerCase();
+			if(secret == '') secret = false;
+		}
+
+		// create a key used to encrypt the board's key before emailing it
+		var salt			=	(secret || '') + ':throw the NSA down the well';
+		var encrypting_key	=	tcrypt.key(encrypting_pass, salt, {keySize: 256/32, iterations: 400});
+
+		// don't do encryption directly, use the Protected model.
+		var keymodel		=	new InviteKey();
+		keymodel.key		=	encrypting_key;
+		keymodel.set({body: encrypted_key});
+		return keymodel.get('key');
+	}
 });
 
 var InviteKey = Protected.extend({
@@ -14,22 +67,10 @@ var BoardInvite = Invite.extend({
 		// make sure we have an email (kinda)
 		if(!this.get('email', '').clean().match(/@/)) return false;
 
-		if(typeOf(secret) == 'string')
-		{
-			secret	=	secret.clean();
-			secret	=	secret.toLowerCase();
-			if(secret == '') secret = false;
-		}
-
-		// create a key used to encrypt the board's key before emailing it
-		var salt			=	(secret || '') + ':throw the NSA down the well';
-		var encrypting_pass	=	tcrypt.uuid();
-		var encrypting_key	=	tcrypt.key(encrypting_pass, salt, {keySize: 256/32, iterations: 400});
-
-		var keymodel		=	new InviteKey({key: tcrypt.key_to_string(board.key)});
-		keymodel.key		=	encrypting_key;
-
-		var encrypted_key	=	keymodel.toJSON().body;
+		var encdata			=	this.encrypt_key(board.key, secret, {normalize: true});
+		var encrypted_key	=	encdata.encrypted_key;
+		var encrypting_pass	=	encdata.encrypting_pass;
+		var used_secret		=	encdata.used_secret;
 
 		from_persona.get_challenge({
 			success: function(challenge) {
@@ -39,7 +80,7 @@ var BoardInvite = Invite.extend({
 					to: this.get('email'),
 					key: encrypting_pass,
 					board_key: encrypted_key,
-					used_secret: !!secret
+					used_secret: used_secret ? 1 : 0
 				}, {
 					success: function(invite) {
 						if(invite.priv)
@@ -72,3 +113,8 @@ var BoardInvite = Invite.extend({
 		});
 	}
 });
+
+var Invites	=	Composer.Collection.extend({
+	model: Invite
+});
+
