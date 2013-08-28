0a5ad3db7528620d32e660653e2a6bee0dfab0ca
Andrew Danger Lyon <orthecreedence@gmail.com>
2013-08-28 02:47:00 -0500

    fixing setup url

--- turtl/setup.js
+++ turtl/setup.js
@@ -2,7 +2,7 @@ Composer.suppress_warnings = true;
 if(window.chrome && window.chrome.extension)
 {
 	window._in_ext				=	true;
-	window._base_url			=	chrome.extension.getURL('/app');
+	window._base_url			=	chrome.extension.getURL('/data/app');
 	window.port					=	new ChromeAddonPort();
 	window.__api_url			=	config.api_url;
 	window._disable_api_tracker	=	true;
