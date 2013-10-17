1c23d25d42dad519aa42f65c1603c053d2b915f9
Andrew Danger Lyon <orthecreedence@gmail.com>
2013-10-16 22:47:23 -0700

    some sync model changes/comment updates

--- models/sync.js
+++ models/sync.js
@@ -2,9 +2,16 @@ var SyncError	=	extend_error(Error, 'SyncError');
 
 var Sync = Composer.Model.extend({
 
-	// holds collections/models that will monitor their respective local db
-	// table for changes and update themselves/their models accordingly
+	// holds collections/models that monitor their respective local db table for
+	// remote changes and sync those changes to in-memory models. note that
+	// local trackers also listen for changes that are made locally because this
+	// allows syncing between different pieces of an addon (say, a background
+	// process and an app tab). this circumvents the need to do forced API syncs
+	// and cross-process eventing.
 	local_trackers: {},
+
+	// holds collections/models that monitor their respective local db table for
+	// local changes and sync those changes to the API
 	remote_trackers: {},
 
 	register_local_tracker: function(collection_or_model, type)
@@ -20,7 +27,11 @@ var Sync = Composer.Model.extend({
 	{
 		if(!collection_or_model.sync_to_api || typeof collection_or_model.sync_to_api != 'function')
 		{
-			throw new SyncError('Local tracker of type '+ type +' does not have `sync_to_api` function.');
+			throw new SyncError('Remote tracker of type '+ type +' does not have `sync_to_api` function.');
+		}
+		if(!collection_or_model.sync_from_api || typeof collection_or_model.sync_from_api != 'function')
+		{
+			throw new SyncError('Remote tracker of type '+ type +' does not have `sync_from_api` function.');
 		}
 		this.remote_trackers[type]	=	collection_or_model;
 	},
@@ -44,7 +55,7 @@ var Sync = Composer.Model.extend({
 			tracker.sync_to_api();
 		}.bind(this));
 
-		// POST /sync here
+		// POST /sync here, call sync_from_api on all respective remote trackers
 
 		this.sync_local.delay(1000, this);
 	}
