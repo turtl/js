18c3068550f5b2b74110e8888e5041b93b9704fb
Andrew Danger Lyon <orthecreedence@gmail.com>
2013-10-18 13:20:37 -0700

    space/tab issues

--- library/functions.js
+++ library/functions.js
@@ -96,15 +96,15 @@ function get_data_from_querystring(url)
 }
 
 Array.prototype.unique = function() {
-    var a = this.concat();
-    for(var i=0; i<a.length; ++i) {
-        for(var j=i+1; j<a.length; ++j) {
-            if(a[i] === a[j])
-                a.splice(j, 1);
-        }
-    }
+	var a = this.concat();
+	for(var i=0; i<a.length; ++i) {
+		for(var j=i+1; j<a.length; ++j) {
+			if(a[i] === a[j])
+				a.splice(j, 1);
+		}
+	}
 
-    return a;
+	return a;
 };
 
 String.implement({
