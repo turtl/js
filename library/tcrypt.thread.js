43e2d9de61a7bb5619152b1b756e505fe1ac074f
Andrew Danger Lyon <orthecreedence@gmail.com>
2014-03-02 17:00:53 -0800

    tcrypt.threaded converted to use sjcl includes, plus adding encrypt+hash method

--- library/tcrypt.thread.js
+++ library/tcrypt.thread.js
@@ -1,15 +1,6 @@
 importScripts(
 	'./mootools-core-1.4.5-server.js',
-	'./cowcrypt/convert.js',
-	'./cowcrypt/crypto_math.js',
-	'./cowcrypt/biginteger.js',
-	'./cowcrypt/block_cipher.js',
-	'./cowcrypt/hasher.js',
-	'./cowcrypt/sha.js',
-	'./cowcrypt/hmac.js',
-	'./cowcrypt/aes.js',
-	'./cowcrypt/rsa.js',
-	'./cowcrypt/pbkdf2.js',
+	'./sjcl.js',
 	'./tcrypt.js' 
 );
 
@@ -25,6 +16,11 @@ self.addEventListener('message', function(e) {
 		{
 			case 'encrypt':
 				res	=	tcrypt.encrypt(key, data, options);
+				break;
+			case 'encrypt+hash':
+				var enc		=	tcrypt.encrypt(key, data, options);
+				var hash	=	tcrypt.hash(enc);
+				res			=	{c: enc, h: hash};
 				break;
 			case 'decrypt':
 				res	=	tcrypt.decrypt(key, data, options);
