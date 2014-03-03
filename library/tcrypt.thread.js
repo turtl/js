00b84807f179af0161867977e83afb4c018e661c
Andrew Danger Lyon <orthecreedence@gmail.com>
2014-03-02 17:46:36 -0800

    enable CBC in tcrypt.threaded, adding stack traces to threaded error response

--- library/tcrypt.thread.js
+++ library/tcrypt.thread.js
@@ -4,6 +4,9 @@ importScripts(
 	'./tcrypt.js' 
 );
 
+// we need CBC for backwards compat
+sjcl.beware['CBC mode is dangerous because it doesn\'t protect message integrity.']();
+
 self.addEventListener('message', function(e) {
 	var cmd		=	e.data.cmd;
 	var key		=	e.data.key;
@@ -32,7 +35,11 @@ self.addEventListener('message', function(e) {
 	}
 	catch(e)
 	{
-		res	=	{type: 'error', data: e.message};
+		var stack = e.stack.replace(/^[^\(]+?[\n$]/gm, '')
+			.replace(/^\s+at\s+/gm, '')
+			.replace(/^Object.<anonymous>\s*\(/gm, '{anonymous}()@')
+			.split('\n');
+		res	=	{type: 'error', data: e.message, trace: stack};
 	}
 
 	if(!res) res = {type: 'null'};
