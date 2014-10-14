682a7911a2abc6904c4446e91806c717bf95a46b
Andrew Danger Lyon <orthecreedence@gmail.com>
2014-10-13 23:38:40 -0700

    adding left/right slide actions for board index/board view interactions.

--- handlers/boards.js
+++ handlers/boards.js
@@ -6,12 +6,16 @@ var boards = {
 			turtl.controllers.pages.trigger('loaded');
 			return;
 		}
-		turtl.controllers.pages.load(BoardsController, {});
+		turtl.controllers.pages.load(BoardsController, {}, {
+			slide: turtl.controllers.pages.is(BoardsViewController) ? 'right' : null
+		});
 	},
 
 	view: function(board_id)
 	{
-		turtl.controllers.pages.load(BoardsViewController, {board_id: board_id});
+		turtl.controllers.pages.load(BoardsViewController, {board_id: board_id}, {
+			slide: turtl.controllers.pages.is(BoardsController) ? 'left' : null
+		});
 	}
 };
 
