929bcaccb7a1ce5f3f6c3c89a6d3e90f81d66664
Andrew Danger Lyon <orthecreedence@gmail.com>
2015-05-20 21:43:39 -0700

    adding wipe local data setting

--- controllers/settings/index.js
+++ controllers/settings/index.js
@@ -1,6 +1,10 @@
 var SettingsController = Composer.Controller.extend({
 	class_name: 'settings',
 
+	events: {
+		'click a[href=#wipe]': 'wipe_data'
+	},
+
 	init: function()
 	{
 		turtl.push_title('Your settings');
@@ -14,6 +18,19 @@ var SettingsController = Composer.Controller.extend({
 		this.html(view.render('settings/index', {
 			user: turtl.user.toJSON()
 		}));
+	},
+
+	wipe_data: function(e)
+	{
+		if(e) e.stop();
+
+		if(!confirm('This will erase all your local data and log you out. Your profile will be downloaded again next time you log in. Continue?')) return;
+		turtl.wipe_local_db()
+			.then(function() { return turtl.user.logout(); })
+			.catch(function(err) {
+				turtl.events.trigger('ui-error', 'There was a problem clearing your profile', err);
+				log.error('settings: wipe db: ', derr(err));
+			});
 	}
 });
 
