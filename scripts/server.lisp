dd1adda59dca9727e6c5e683bac12ff19c13d9d6
Andrew Danger Lyon <orthecreedence@gmail.com>
2015-05-22 20:18:06 -0700

    implementing new wookie serve-html5-app helper

--- scripts/server.lisp
+++ scripts/server.lisp
@@ -4,29 +4,5 @@
   (:use :cl :wookie :wookie-plugin-export))
 (in-package :turtl-server)
 
-(load-plugins)
-
-(defun file-contents (path)
-  "Sucks up an entire file from PATH into a freshly-allocated string,
-   returning two values: the string and the number of bytes read."
-  (with-open-file (s path)
-    (let* ((len (file-length s))
-           (data (make-string len)))
-      (values data (read-sequence data s)))))
-
-(defun load-index (res)
-  (let ((body (file-contents "index.html")))
-    (send-response res :body body :headers '(:content-type "text/html"))))
-
-(defroute (:get "/") (req res)
-  (load-index res))
-
-(def-directory-route "/" "./")
-
-(defroute (:get ".*") (req res)
-  (load-index res))
-
-(as:with-event-loop (:catch-app-errors t)
-  (vom:config :wookie :info)
-  (start-server (make-instance 'listener :port 8185)))
+(wookie-helper:serve-html5-app :port 8185)
 
