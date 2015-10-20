f0214b4c9939aad26b1984652cb7664b422e84d1
Andrew Danger Lyon <orthecreedence@gmail.com>
2015-10-20 01:24:00 -0700

    sharing: disable swiping when coming from notes controller

--- handlers/sharing.js
+++ handlers/sharing.js
@@ -2,7 +2,7 @@ var sharing = {
 	index: function()
 	{
 		var slide = false;
-		if(turtl.controllers.pages.is(NotesIndexController)) slide = 'right';
+		//if(turtl.controllers.pages.is(NotesIndexController)) slide = 'right';
 		turtl.back.clear();
 		turtl.controllers.pages.load(SharingController, {}, {
 			slide: slide
