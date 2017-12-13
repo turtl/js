36a509640205736d5bf759cf880c58440e56f41a
Andrew Danger Lyon <orthecreedence@gmail.com>
2017-12-12 23:30:04 -0800

    delete account: adding spinner

--- controllers/settings/delete_account.js
+++ controllers/settings/delete_account.js
@@ -1,4 +1,9 @@
 var DeleteAccountController = FormController.extend({
+	elements: {
+		'input[type=submit]': 'inp_submit',
+		'p.load': 'el_loader',
+	},
+
 	events: {
 		'submit form': 'do_it'
 	},
@@ -24,13 +29,23 @@ var DeleteAccountController = FormController.extend({
 		if(e) e.stop();
 		if(!confirm(i18next.t('Just making sure...do you really want to delete your account?'))) return;
 
+		this.el_loader.addClass('active');
+		this.inp_submit.set('disabled', 'disabled');
+		turtl.loading(true);
+
 		turtl.user.delete_account()
+			.bind(this)
 			.then(function() {
 				barfr.barf(i18next.t('Your account has been deleted.'));
 			})
 			.catch(function(err) {
 				turtl.events.trigger('ui-error', i18next.t('There was a problem deleting your account. Please try again.'), err);
 				log.error('account: delete: ', turtl.user.id(), derr(err));
+			})
+			.finally(function() {
+				turtl.loading(false);
+				this.inp_submit.set('disabled', '');
+				this.el_loader.removeClass('active');
 			});
 	}
 });
