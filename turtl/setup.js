96869c152b5089b9717add725935e5d46b39d23f
Andrew Danger Lyon <orthecreedence@gmail.com>
2018-01-08 20:33:30 -0800

    fixing desktop version sniffing in setup script

--- turtl/setup.js
+++ turtl/setup.js
@@ -1,13 +1,9 @@
 Composer.suppress_warnings = true;
 
-// we need CBC for backwards compat
-sjcl.beware['CBC mode is dangerous because it doesn\'t protect message integrity.']();
-
 setTimeout(function() {
-	if(new String(config.client) == 'desktop')
-	{
-		var gui = require('nw.gui');
-		config.version = gui.App.manifest.version;
+	if(new String(config.client) == 'desktop') {
+		var app = require('electron').remote.app;
+		config.version = app.getVersion();
 	}
 });
 
