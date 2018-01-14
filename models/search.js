d66fa6f8456e3fc2859146ea61e748bca7933dab
Andrew Danger Lyon <orthecreedence@gmail.com>
2018-01-14 13:36:57 -0800

    adding find_tags method to search

--- models/search.js
+++ models/search.js
@@ -1,8 +1,7 @@
 var Search = Composer.Collection.extend({
 	total: 0,
 
-	search: function(search, options)
-	{
+	search: function(search, options) {
 		search || (search = {});
 		options || (options = {});
 
@@ -43,6 +42,45 @@ var Search = Composer.Collection.extend({
 				this.trigger('search-done', notes, tags, total);
 				this.trigger('search-tags', tags);
 				return [notes, tags, total];
+			});
+	},
+
+	find_tags: function(search, options) {
+		search || (search = {});
+		options || (options = {});
+
+		// create a search object rust understands. it's only slightly different
+		// from the js version, but there is some conversion and i don't want to
+		// rewrite all the js queries, so here we are.
+		var core_search = {
+			text: search.text,
+			space_id: search.space,
+			boards: search.board ? [search.board] : null,
+			tags: search.tags,
+			exclude_tags: search.exclude_tags,
+			type: search.type,
+			has_file: search.has_file,
+			color: search.color,
+			sort: (search.sort || [])[0],
+			sort_direction: (search.sort || [])[1],
+			page: search.page,
+			per_page: search.per_page,
+		};
+		Object.keys(core_search).forEach(function(k) {
+			if(
+				core_search[k] === null ||
+				core_search[k] === undefined ||
+				(Array.isArray(core_search[k]) && core_search[k].length == 0) ||
+				(k == 'text' && core_search[k] == '')
+			) {
+				delete core_search[k];
+			}
+		});
+		return turtl.core.send('profile:find-tags', core_search)
+			.bind(this)
+			.then(function(search_data) {
+				var tags = search_data.tags.map(function(t) { return {name: t[0], count: t[1]}; });
+				return tags;
 			});
 	},
 });
