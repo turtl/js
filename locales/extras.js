71a8b52505e71322513954d718c244f27a2ae160
Andrew Danger Lyon <orthecreedence@gmail.com>
2018-02-11 15:05:25 -0800

    adding locales/extras.js, which holds entries that arent in the UI but are included in other components.

---
+++ locales/extras.js
@@ -0,0 +1,14 @@
+/**
+ * This is a file that holds locale entries for items that are not included in
+ * the UI.
+ *
+ * This can be handy if another component loads the UI and has localization it
+ * wants to do. Ideally, that component would have its own localization, but to
+ * be frank it seems stupid to not store it in one place, and the UI is the
+ * common piece here, so we just put it here and be done with it.
+ *
+ * Having these entries here makes sure that `scripts/i18n-tool` will find them
+ * when generating locale templates for us.
+ */
+i18next.t('New bookmark');
+
